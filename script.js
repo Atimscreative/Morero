f8017a915d7659948a64f40436f2bf8c962c6c7d
atimscreative <timilehinsunday144@gmail.com>
2024-09-21 10:16:55 +0100

    feature section added

--- script.js
+++ script.js
@@ -3,15 +3,51 @@ const openModal = document.querySelector("#open"),
   overlay = document.querySelector("#overlay"),
   mobileMenu = document.querySelector("#mobile-menu");
 
-openModal.addEventListener("click", () => {
-  overlay.classList.add("right-0", "duration-300");
-  mobileMenu.classList.add("right-0", "duration-300");
-  console.log("CLicked");
-});
+const tabs = document.querySelector("[role='tablist']");
+console.log(tabs);
 
-closeModal.addEventListener("click", () => {
-  overlay.classList.remove("right-0", "duration-300");
-  mobileMenu.classList.remove("right-0", "duration-300");
-});
+/* ===================================== */
+/* ===========-- FUNCTIONS --========== */
+/* =================================== */
 
-function showModal() {}
+// MOBILE MENU CLOSE
+function modalClose() {
+  // ADD HIDE MENU CLASSNAME
+  overlay.classList.add("hide-menu");
+  mobileMenu.classList.add("hide-menu");
+
+  // REMOVE SHOW MENU CLASSNAME
+  overlay.classList.remove("show-menu");
+  mobileMenu.classList.remove("show-menu");
+  // console.log("CLicked", ...overlay.classList);
+}
+
+function modalOpen() {
+  // REMOVE HIDE MENU CLASSNAME
+  overlay.classList.remove("hide-menu");
+  mobileMenu.classList.remove("hide-menu");
+
+  // ADD SHOW MENU CLASSNAME
+  overlay.classList.add("show-menu");
+  mobileMenu.classList.add("show-menu");
+  // console.log("CLicked", ...overlay.classList);
+}
+
+/* =================================== */
+/* =========== EVENT LISTENERS========== */
+/* =================================== */
+
+// OPEN MODAL WHEN USER CLICK MENU ICON
+openModal.addEventListener("click", modalOpen);
+
+// CLOSE MODAL WHEN USER CLICK X ICON
+closeModal.addEventListener("click", modalClose);
+
+// CLOSE MODAL WHEN USER CLICK THE MOBILE OVERLAY
+overlay.addEventListener("click", modalClose);
+
+tabs.addEventListener("click", (e) => {
+  e.preventDefault();
+  // if(e.target)
+  console.log(e, e.target, e.currentTarget, e.target.closest("button"));
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,51 @@ module.exports = {
         inter: `"Inter", sans-serif`,
       },
       backgroundImage: {
-        'hero': "url('/images/hero-bg.avif')",
-      }
+        hero: "url('/images/hero-bg.avif')",
+      },
+      colors: {
+        main: {
+          50: "#fef8ee",
+          100: "#feefd6",
+          200: "#fcdbac",
+          300: "#f9c078",
+          400: "#f69b41",
+          500: "#f37e1c",
+          600: "#d55d11", // main
+          700: "#bd4b11",
+          800: "#963c16",
+          900: "#793315",
+          950: "#411809",
+        },
+        secondary: {
+          50: "#f3faf9",
+          100: "#d6f1ea",
+          200: "#ace3d7",
+          300: "#7bcdbe",
+          400: "#50b1a3",
+          500: "#369689",
+          600: "#29786f",
+          700: "#24615b",
+          800: "#214e4a", // main
+          900: "#1f423e",
+          950: "#0d2624",
+        },
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    require("@tailwindcss/forms"),
+    function ({ addUtilities }) {
+      addUtilities({
+        ".no-scrollbar": {
+          overflow: "scroll",
+          "-ms-overflow-style": "none", // IE and Edge
+          "scrollbar-width": "none", // Firefox
+        },
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none", // Chrome, Safari, and Edge
+        },
+      });
+    },
   ],
 };
